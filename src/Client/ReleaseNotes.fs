28ba72a0a1e29395511c89af23813149270e7c57
Melissa S Smith <melissasmith358@gmail.com>
2018-11-23 15:08:45 -0800

    Bumping version to 0.0.7

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -5,18 +5,6 @@ let Version = 0.0.7
 let IsPrerelease = false
 
 let Notes = 
-### 0.0.1 - 2018-11-13
-* Initial Release
-
-### 0.0.2 - 2018-11-23
-* Bumping Release Number
-
-### 0.0.3 - 2018-11-23
-* Bumping Release Number
-
-### 0.0.4 - 2018-11-23
-* Bumping Release Number
-
 ### 0.0.5 - 2018-11-23
 * Fix gitignore file
 
