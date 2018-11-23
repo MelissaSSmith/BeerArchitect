9c62922d5ebd2c6d7b17be5ae01f68c37fe6eacd
Melissa S Smith <melissasmith358@gmail.com>
2018-11-23 15:09:55 -0800

    Bumping version to 0.0.7

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,8 +1,5 @@
-### 0.0.5 - 2018-11-23
-* Fix gitignore file
-
-### 0.0.6 - 2018-11-23
-* Fix client csproj
-
 ### 0.0.7 - 2018-11-23
 * Fix Dockerfile
+
+### 0.0.7 - 2018-11-23
+* Bumping Release
--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -5,12 +5,9 @@ let Version = 0.0.7
 let IsPrerelease = false
 
 let Notes = 
-### 0.0.5 - 2018-11-23
-* Fix gitignore file
-
-### 0.0.6 - 2018-11-23
-* Fix client csproj
-
 ### 0.0.7 - 2018-11-23
 * Fix Dockerfile
+
+### 0.0.7 - 2018-11-23
+* Bumping Release
 
