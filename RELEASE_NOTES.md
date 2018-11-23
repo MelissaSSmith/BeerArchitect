008f3ab4c76a41b6c75d8c20688c3a2147b53b50
Melissa S Smith <melissasmith358@gmail.com>
2018-11-23 12:03:52 -0800

    Bumping version to 0.0.2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
 ### 0.0.1 - 2018-11-13
 * Initial Release
+
+### 0.0.2 - 2018-11-23
+* Bumping Release Number
--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,10 +1,13 @@
 module internal ReleaseNotes
 
-let Version = 0.0.1
+let Version = 0.0.2
 
 let IsPrerelease = false
 
 let Notes = 
 ### 0.0.1 - 2018-11-13
 * Initial Release
+
+### 0.0.2 - 2018-11-23
+* Bumping Release Number
 
