1e4ff57e123c97fae7a7ccf9f1aaf10a9581cd65
Melissa S Smith <melissasmith358@gmail.com>
2019-01-19 11:28:54 -0800

    Start calculations

--- src/Server/AllGrainCalculator.fs
+++ src/Server/AllGrainCalculator.fs
@@ -4,11 +4,46 @@ open Giraffe
 open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks.V2
 
+open ServerCode.AbvCalculator
+open ServerCode.Fermentables
 open Shared
+
+let getAttenuation yeastTolerance =
+    match yeastTolerance with
+    | YeastTolerance.Low ->
+        66.0
+    | YeastTolerance.Medium ->
+        72.0
+    | YeastTolerance.High ->
+        77.0
+
+let specificGravity gravUnits =
+    gravUnits / 1000.0 + 1.0 
+
+let gravityPerLbs mashEff potentialExtract =
+    mashEff * potentialExtract
+    
+let ingredientGravity lbsNeeded gravPerLbs =
+    lbsNeeded * gravPerLbs
+
+let estimatedOriginalGravity targetOg boilSize batchSize =
+    (targetOg * boilSize) / batchSize
+
+let estimatedFinalGravity totalGravPoints attenuationPercent =
+    (1.0  - attenuationPercent) * totalGravPoints
+    |> specificGravity
 
 let calculate : HttpHandler =
     fun (next : HttpFunc) (ctx : HttpContext) ->
         task {
-            let allGrainResult = { EstPreBoilOG = 1.0; EstOG = 1.0; EstFG = 1.0; EstABV = 1.0 }
+            let! allGrainInput = ctx.BindJsonAsync<AllGrainInput>()
+
+            let fermentableList = getAllFermentablesFromFile
+            let attenuationPercent = getAttenuation allGrainInput.YeastTolerance
+
+            let og = estimatedOriginalGravity 0.0 allGrainInput.PreBoilSize allGrainInput.BatchSize
+            let fg = estimatedFinalGravity 0.0 attenuationPercent
+            let abv = Abv og fg
+            let allGrainResult = { EstPreBoilOG = 1.0; EstOG = og; EstFG = fg; EstABV = abv }
             return! ctx.WriteJsonAsync allGrainResult
         }
