c3ea7ab1fb4ed3475359238b04933ad273dfbac9
Melissa S Smith <melissasmith358@gmail.com>
2019-02-10 12:49:36 -0800

    Bumping version to 0.0.15

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
 ### 0.0.14 - 2019-02-10
 * Add All-Grain calculator, Yeast Profiles, and Dilution & Boil-Off Calculator
+
+### 0.0.15 - 2019-02-10
+* Fix Issue with Release
--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,10 +1,13 @@
 module internal ReleaseNotes
 
-let Version = 0.0.14
+let Version = 0.0.15
 
 let IsPrerelease = false
 
 let Notes = 
 ### 0.0.14 - 2019-02-10
 * Add All-Grain calculator, Yeast Profiles, and Dilution & Boil-Off Calculator
+
+### 0.0.15 - 2019-02-10
+* Fix Issue with Release
 
